cfee52826cf82944c6db8ef21fcc126ab1bb46fb
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-01-12 19:15:55 +0100

    fix alenment

--- resources/views/calendar/menu.blade.php
+++ resources/views/calendar/menu.blade.php
@@ -12,9 +12,12 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
         </a>
     </div>
-    <div class="week-days-overview">
-        @foreach($weekDays as $weekDay)
-            <div class="week-day-overview">{{ $weekDay }}</div>
-        @endforeach
+
+    <div class="week-days-overview-wrapper">
+        <div class="week-days-overview">
+            @foreach($weekDays as $weekDay)
+                <div class="week-day-overview">{{ $weekDay }}</div>
+            @endforeach
+        </div>
     </div>
 </div>
