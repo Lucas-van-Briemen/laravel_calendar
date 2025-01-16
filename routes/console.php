8cff6ad82ea65c5bc250b3d35fd21d9b21fbdb79
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-01-16 22:52:39 +0100

    add cronjob

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+// Send an email if an agenda item is in 30 minutes
+Artisan::command('agenda-item-reminder', function () {
+    // Get all agenda items that are in 30 minutes
+    $agendaItems = AgendaItem::where('start', '<=', now()->addMinutes(30))
+        ->where('start', '>=', now())
+        ->get();
+
+        $this->info('Found ' . $agendaItems->count() . ' agenda items');
+})->everyMinute();
