ce428cdcb3790ee081f6fd6dfcafeeacf2755670
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-01-17 22:17:06 +0100

    add icons and allow for viewing

--- resources/views/calendar/calendar.blade.php
+++ resources/views/calendar/calendar.blade.php
@@ -30,18 +30,26 @@
             <div class="agenda-item"
                 style="grid-row: {{ $agendaItem["startRow"] }} / {{ $agendaItem["endRow"] }}; grid-column: {{ $agendaItem["startColumn"] }} / {{ $agendaItem["endColumn"] }};"
             >
-                <h1 class="agenda-item-title">{{ $agendaItem["title"] }}</h1>
-                <p class="agenda-item-description">{{ $agendaItem["description"] }}</p>
 
-                <div class="agenda-item-time">
+                <div class="agenda-item-details">
+                    <h1 class="agenda-item-title">{{ $agendaItem["title"] }}</h1>
+                    <p class="agenda-item-description">{{ $agendaItem["description"] }}</p>
+
+                    <div class="agenda-item-time">
+                </div>
                 </div>
 
                 <div class="agenda-item-actions">
+                    <a href="{{ route('agenda-items.show', ['agenda_item' => $agendaItem['id']]) }}">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                    </a>
+
+
                     <form action="{{ route('agenda-items.destroy', ['agenda_item' => $agendaItem['id']]) }}" method="POST">
                         @csrf
                         @method('delete')
                         <button type="submit">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+                            <i class="fa-solid fa-trash"></i>
                         </button>
                     </form>
                 </div>
