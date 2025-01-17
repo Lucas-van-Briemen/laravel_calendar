a2145be0d9cf76b4b2b89689c5d1c80f186ff84a
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-01-17 22:36:55 +0100

    fix the hour offset

--- routes/console.php
+++ routes/console.php
@@ -12,9 +12,7 @@ Artisan::command('inspire', function () {
 // Send an email if an agenda item is in 30 minutes
 Artisan::command('agenda-item-reminder', function () {
     // Get all agenda items that are in 30 minutes
-    $agendaItems = AgendaItem::where('start', '<=', now()->addMinutes(30))
-        ->where('start', '>=', now())
+    $agendaItems = AgendaItem::where('start', '<=', now()->addMinutes(90))
+        ->where('start', '>=', now()->addMinutes(60))
         ->get();
-
-        $this->info('Found ' . $agendaItems->count() . ' agenda items that start between: ' . now()->format('Y-m-d H:i:s')) . ' and ' . now()->addMinutes(30)->format('Y-m-d H:i:s');
 })->everyMinute();
