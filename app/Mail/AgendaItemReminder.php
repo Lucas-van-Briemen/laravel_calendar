3ea33f296526bfa7fbaf174a43d9516ae2fc7728
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-01-18 20:18:32 +0100

    fix email nameing

--- app/Mail/AgendaItemReminder.php
+++ app/Mail/AgendaItemReminder.php
@@ -39,7 +39,7 @@ class AgendaItemReminder extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.agenda-item-reminder',
             with: [
                 'agendaItem' => $this->agendaItem,
             ],
