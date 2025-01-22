3121e1bd841409d128a3968dcb29e6140d4bd57f
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-01-22 20:00:47 +0100

    add the magenger header

--- resources/views/calendar/manager.blade.php
+++ resources/views/calendar/manager.blade.php
@@ -1,8 +1,10 @@
 <div class="calendar-manager">
     <div class="calendar-manager-header">
         <div class="calendar-manager-blob"></div>
-        <div>{{ __('Display') }}</div>
-        <div>{{ __('Info') }}</div>
+        <div class='calendar-manager-active calendar-manager-header-item'>{{ __('Display') }}</div>
+        <div class='calendar-manager-header-item'>{{ __('Info') }}</div>
     </div>
 
 </div>
+
+@vite('resources/js/calendar-manager.js')
