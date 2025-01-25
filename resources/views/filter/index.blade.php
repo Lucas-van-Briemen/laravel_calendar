46f20d1597535a34efd5c2e6900a59a822acf7b9
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-01-25 21:05:54 +0100

    work on the functions

--- resources/views/filter/index.blade.php
+++ resources/views/filter/index.blade.php
@@ -1,1 +1,28 @@
-test
+<x-app-layout>
+
+    @vite('resources/css/filter/filter.css')
+
+    <div class="filter-wrapper">
+        @foreach ($filters as $filter)
+            <div class="filter-item">
+                <h1 class="filter-header">{{ $filter->name }}</h1>
+                <div class='filter-color'>
+                    {{ $filter->color }}
+                    <form action="{{ route('manage-filters.update', $filter->id) }}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <input type="hidden" name="color" value="{{ $filter->color }}">
+                        <button type="submit"><i class="fa fa-paint-brush"></i></button>
+                    </form>
+                </div>
+
+                <div class="filter-delete">
+                    <form action="{{ route('manage-filters.destroy', $filter->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit"><i class="fa fa-trash"></i></button>
+                    </form>
+                </div>
+            </div>
+        @endforeach
+</x-app-layout>
